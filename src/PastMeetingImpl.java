e3881312776ff20d3a2dd6f0418672ff92de80e1
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 18:32:24 +0000

    Changed PastMeetingImpl to be subclass of MeetingImpl, with additional method getNotes()

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -1,32 +1,14 @@
 import java.util.Calendar;
 import java.util.Set;
 
-public class PastMeetingImpl implements PastMeeting {
-	private Calendar meetingDate;
-	private int id;
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	private String notes;
-	private Set<Contact> contactList;
-	
+
 	public PastMeetingImpl(Set<Contact> contacts, Calendar date, String notes){
-		this.meetingDate = date;
+		super(contacts,date);
 		this.notes = notes;
-		this.contactList = contacts;
-		//need to change this
-		this.id = 1;
 	}
 	
-	public int getId() {
-		return id;
-	}
-
-	public Calendar getDate() {
-		return meetingDate;
-	}
-
-	public Set<Contact> getContacts() {
-		return contactList;
-	}
-
 	public String getNotes() {
 		return notes;
 	}
