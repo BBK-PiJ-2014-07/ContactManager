db340c15728b6826616eb6d10a1abaee8d309773
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 13:25:48 +0000

    Created skeleton of ContactImpl. No method content yet.

---
+++ src/ContactImpl.java
@@ -0,0 +1,29 @@
+
+public class ContactImpl implements Contact {
+
+	public ContactImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addNotes(String note) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
