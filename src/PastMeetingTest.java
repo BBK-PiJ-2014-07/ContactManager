7a14c2ea3cab20730336be3c733b015b6e8a30ec
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 17:36:53 +0000

    Added buildup method of PastMeetingTest

--- src/PastMeetingTest.java
+++ src/PastMeetingTest.java
@@ -1,14 +1,16 @@
-
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PastMeetingTest {
+	private PastMeeting pm;
 
 	@Before
 	public void buildUp(){
-		//buildup method
+		Calendar cal = new GregorianCalendar(2013,12,01);
+		pm = new PastMeetingImpl(cal, "Meeting notes");
 	}
 	
-
 
 }
