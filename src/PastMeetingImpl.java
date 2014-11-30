e73c6653919ce4bd384e22cb90f5eaf461e81947
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 17:43:48 +0000

    Wrote getDate(). testGetDate() now passes.

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -20,8 +20,7 @@ public class PastMeetingImpl implements PastMeeting {
 	}
 
 	public Calendar getDate() {
-		// TODO Auto-generated method stub
-		return null;
+		return meetingDate;
 	}
 
 	public Set<Contact> getContacts() {
