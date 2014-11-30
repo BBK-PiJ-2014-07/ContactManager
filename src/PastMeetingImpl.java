2378988b5654ed8539213dd8b20d1b8ecad4bb46
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 17:35:48 +0000

    Added initial constructor and attributes to PastMeetingImpl.

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -1,9 +1,20 @@
 import java.util.Calendar;
 import java.util.Set;
-
+import java.util.GregorianCalendar;
 
 public class PastMeetingImpl implements PastMeeting {
-
+	private Calendar meetingDate;
+	private int id;
+	private String notes;
+	private Set<Contact> contactList;
+	
+	public PastMeetingImpl(Calendar date, String notes){
+		this.meetingDate = date;
+		this.notes = notes;
+		this.contactList = null;
+		this.id = 1;
+	}
+	
 	public int getId() {
 		// TODO Auto-generated method stub
 		return 0;
