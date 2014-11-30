07712402318f4a33744c9c0fdfcdaf82b767e966
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 13:20:20 +0000

    Built initial ContactTest class for testing ContactImpl.java. First skeleton of first test.

---
+++ src/ContactTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class ContactTest {
+	@Before
+	public void buildUp(){
+		//open the file, create details
+	}
+	
+	@Test
+	public void testGetID(){
+		//test ID getter
+	}
+	
+	@After
+	public void closeDown(){
+		//close the file
+	}
+}
