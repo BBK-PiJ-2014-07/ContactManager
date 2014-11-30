cc4187d9838a14d31fc9b16e1c60b8c52afad8be
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-30 13:58:35 +0000

    Created PastMeetingImpl skeleton.

---
+++ src/PastMeetingImpl.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+import java.util.Set;
+
+
+public class PastMeetingImpl implements PastMeeting {
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
