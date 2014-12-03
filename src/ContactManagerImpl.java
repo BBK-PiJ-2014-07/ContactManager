f2c9e5764fbf4f44d96687497a0cbbfc6bbe1ace
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-12-03 16:16:11 +0000

    Added comments to methods in the interest of avoiding future headaches.

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -25,70 +25,195 @@ public class ContactManagerImpl implements ContactManager {
 		}
 	}
 
-
+	/**
+	 * The method that writes to the file. It needs to check whether the file exists, and if so, append
+	 * rather than overwrite. 
+	 * @param str - the string to be written
+	 */
+	public void writeToFile(String str){
+		//TODO
+	}
+	
+	/**
+	* Add a new meeting to be held in the future.
+	*
+	* @param contacts a list of contacts that will participate in the meeting
+	* @param date the date on which the meeting will take place
+	* @return the ID for the meeting
+	* @throws IllegalArgumentException if the meeting is set for a time in the past,
+	* of if any contact is unknown / non-existent
+	*/
 	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+		//check date of calendar
 		FutureMeeting fm = new FutureMeetingImpl(contacts, date);		
 		//write to file.
 		return fm.getId();
 	}
 
+	/**
+	* Returns the PAST meeting with the requested ID, or null if it there is none.
+	*
+	* @param id the ID for the meeting
+	* @return the meeting with the requested ID, or null if it there is none.
+	* @throws IllegalArgumentException if there is a meeting with that ID happening in the future
+	*/
 	public PastMeeting getPastMeeting(int id) {
-		// TODO Auto-generated method stub
+		// Look through file to see if it exists. 
+		// If it exists, then return it.
 		return null;
 	}
 
+	/**
+	* Returns the FUTURE meeting with the requested ID, or null if there is none.
+	*
+	* @param id the ID for the meeting
+	* @return the meeting with the requested ID, or null if it there is none.
+	* @throws IllegalArgumentException if there is a meeting with that ID happening in the past
+	*/
 	public FutureMeeting getFutureMeeting(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	* Returns the meeting with the requested ID, or null if it there is none.
+	*
+	* @param id the ID for the meeting
+	* @return the meeting with the requested ID, or null if it there is none.
+	*/
 	public Meeting getMeeting(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+	/**
+	* Returns the list of future meetings scheduled with this contact.
+	*
+	* If there are none, the returned list will be empty. Otherwise,
+	* the list will be chronologically sorted and will not contain any
+	* duplicates.
+	*
+	* @param contact one of the user’s contacts
+	* @return the list of future meeting(s) scheduled with this contact (maybe empty).
+	* @throws IllegalArgumentException if the contact does not exist
+	*/
 	public List<Meeting> getFutureMeetingList(Contact contact) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	/**
+	* Returns the list of meetings that are scheduled for, or that took
+	* place on, the specified date
+	*
+	* If there are none, the returned list will be empty. Otherwise,
+	* the list will be chronologically sorted and will not contain any
+	* duplicates.
+	*
+	* @param date the date
+	* @return the list of meetings
+	*/
 	public List<Meeting> getFutureMeetingList(Calendar date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+	/**
+	* Returns the list of past meetings in which this contact has participated.
+	*
+	* If there are none, the returned list will be empty. Otherwise,
+	* the list will be chronologically sorted and will not contain any
+	2* duplicates.
+	*
+	* @param contact one of the user’s contacts
+	* @return the list of future meeting(s) scheduled with this contact (maybe empty).
+	* @throws IllegalArgumentException if the contact does not exist
+	*/
 	public List<PastMeeting> getPastMeetingList(Contact contact) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	* Create a new record for a meeting that took place in the past.
+	*
+	* @param contacts a list of participants
+	* @param date the date on which the meeting took place
+	* @param text messages to be added about the meeting.
+	* @throws IllegalArgumentException if the list of contacts is
+	* empty, or any of the contacts does not exist
+	* @throws NullPointerException if any of the arguments is null
+	*/
 	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
 			String text) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	* Add notes to a meeting.
+	*
+	* This method is used when a future meeting takes place, and is
+	* then converted to a past meeting (with notes).
+	*
+	* It can be also used to add notes to a past meeting at a later date.
+	*
+	* @param id the ID of the meeting
+	* @param text messages to be added about the meeting.
+	* @throws IllegalArgumentException if the meeting does not exist
+	* @throws IllegalStateException if the meeting is set for a date in the future
+	* @throws NullPointerException if the notes are null
+	*/
 	public void addMeetingNotes(int id, String text) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	
+	/**
+	* Create a new contact with the specified name and notes.
+	*
+	* @param name the name of the contact.
+	* @param notes notes to be added about the contact.
+	* @throws NullPointerException if the name or the notes are null
+	*/
 	public void addNewContact(String name, String notes) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	* Returns a list containing the contacts that correspond to the IDs.
+	*
+	* @param ids an arbitrary number of contact IDs
+	* @return a list containing the contacts that correspond to the IDs.
+	* @throws IllegalArgumentException if any of the IDs does not correspond to a real contact
+	*/
 	public Set<Contact> getContacts(int... ids) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	/**
+	* Returns a list with the contacts whose name contains that string.
+	3*
+	* @param name the string to search for
+	* @return a list with the contacts whose name contains that string.
+	* @throws NullPointerException if the parameter is null
+	*/
 	public Set<Contact> getContacts(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	/**
+	* Save all data to disk.
+	*
+	* This method must be executed when the program is
+	* closed and when/if the user requests it.
+	*/
 	public void flush() {
 		// TODO Auto-generated method stub
 
