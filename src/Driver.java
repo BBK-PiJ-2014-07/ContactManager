20f3c18cf8a6b013859aea7b1dc3ea9524c338b3
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-12-03 15:30:02 +0000

    Created Driver class for the IO and running of ContactManager

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
+
+public class Driver {
+
+	public Driver() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
